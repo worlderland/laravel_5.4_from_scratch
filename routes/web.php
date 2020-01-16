919add29717fd23956688100ea4010a1203b2910
Alessandro Coraglia <info.alessandrocoraglia@gmail.com>
2020-01-16 21:51:28 +0800

    End of episode 8.

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,5 @@
 <?php
 
-use App\Task;
+Route::get('/tasks', 'TasksController@index');
 
-Route::get('/tasks', function () {
-
-    //$tasks = DB::table('tasks')->latest()->get(); //DB query to fetch data from database (in this case table tasks)
-
-    $tasks = Task::all();
-
-    return view('tasks.index', compact('tasks'));
-});
-
-
-Route::get('/tasks/{task}', function ($id) {
-
-    $task = Task::find($id);
-
-    
-
-    // dd($task); die and dump function, Laravel helper
-
-    return view('tasks.show', compact('task'));
-});
+Route::get('/tasks/{task}', 'TasksController@show');
